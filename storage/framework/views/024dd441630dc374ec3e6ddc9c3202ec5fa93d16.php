08f807096ddc091022409dc862bbf8e44b600371
kerick jeff <frukerickjeff@gmail.com>
2016-07-15 20:16:47 +0100

    new design homepage design implementation

--- storage/framework/views/024dd441630dc374ec3e6ddc9c3202ec5fa93d16.php
+++ storage/framework/views/024dd441630dc374ec3e6ddc9c3202ec5fa93d16.php
@@ -11,82 +11,79 @@
 
         <link rel="stylesheet" href="<?php echo e(asset('boot/css/bootstrap.min.css')); ?>" media="screen" title="no title" charset="utf-8">
         <link rel="stylesheet" href="<?php echo e(asset('css/sidebar.css')); ?>" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="<?php echo e(asset('css/hover.css')); ?>" media="screen" title="no title" charset="utf-8">
+
+        <style media="screen">
+            #login:hover, #register:hover, #pubs:hover {
+                background: rgba(51,122,183,.2);
+            }
+        </style>
     </head>
     <body>
-        <?php $__env->startSection('header'); ?>
-            <nav class="navbar navbar-default no-margin navbar-fixed-top">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header fixed-brand">
-                    <ul class="nav navbar-nav">
-                        <li class="active" ><button style = "border-style: none;" class="navbar-toggle collapse in" data-toggle="collapse" id="menu-toggle-2" data-target = "#collapsable"> <span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span></button></li>
-                        <a class="navbar-brand" href="/" style = "color: rgba(51,122,183,1); font-size: 48px">iPub</a>
-                    </ul>
-                </div> <!-- bs-example-navbar-collapse-1 -->
-                <div class="collapse navbar-collapse" id = "collapsable">
-                    <ul class="nav navbar-nav">
-                        <li><a href="#"><input type="button" class = "btn btn-primary" name="pubs" value="Pubs"></a></li>
-                    </ul>
-                    <ul class = "nav navbar-nav pull-right">
-                        <li><a href="#"><input type="button" class = "btn btn-primary" name="login" value="Login"></a></li>
-                        <li><a href="#"><input type="button" class = "btn btn-primary" name="register" value="Register"></a></li>
-                    </ul>
+        <div id="wrapper">
+          <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+              <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class = "sr-only">Toggle navigation</span>
+                        <span class = "glyphicon glyphicon-menu-hamburger"></span>
+                    </button>
+                    <div class="navbar-brand">
+                        <a id="menu-toggle" href="#" class="glyphicon glyphicon-align-justify btn-menu toggle">
+                            <i class="fa fa-bars"></i>
+                        </a>
+                        <a href="#" style = "text-decoration: none; color: rgba(51,122,183,1);">iPub</a>
+                    </div>
                 </div>
-            </nav>
-        <?php echo $__env->yieldSection(); ?>
-
-        <div class="container-fluid">
-          <?php $__env->startSection('sidebar'); ?>
-              <div id="wrapper">
-              <!-- Sidebar -->
-                  <div id="sidebar-wrapper">
-                      <ul class="sidebar-nav nav-pills nav-stacked" id="menu">
-                        <li class = "active">
-                            <a href="#"> <span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-inbox"></i></span>Inbox <span class = "badge">4</span> </a>
-                        </li>
-
-                          <li>
-                              <a href="#"> <span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-globe"></i></span>Notifications <span class = "badge">4</span> </a>
-                          </li>
-
-                          <li>
-                              <a href="#"> <span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-user"></i></span>Profile</a>
-                          </li>
-
-                          <li>
-                              <a href="#"><span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-upload"></i></span>Upload File</a>
-                              <ul class="nav-pills nav-stacked" style="list-style-type:none;">
-                                  <li><a href="#"><span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-picture "></i></span>Photo <span class = "badge">124</span> </a></li>
-                                  <li><a href="#"><span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-film"></i></span>Video <span class = "badge">4</span> </a></li>
-                              </ul>
-                          </li>
-
-                          <li>
-                              <a href="#"><span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-cog"></i></span>Settings</a>
-                          </li>
-
-                          <li>
-                              <a href="#"><span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-stats"></i></span>Statistics</a>
-                          </li>
-
-                          <li>
-                              <a href="#"><span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-log-out"></i></span>Logout</a>
-                          </li>
-                      </ul>
-                  </div><!-- /#sidebar-wrapper -->
-
-                  <!-- Page Content -->
-                  <!-- Page Content -->
-                  <div id="page-content-wrapper">
-                      <div class="container-fluid xyz">
-
-                          <?php echo $__env->yieldContent('content'); ?>
-
-                      </div>
-                  </div>
-                  <!-- /#page-content-wrapper -->
+                <div id="navbar" class="collapse navbar-collapse">
+                  <ul class="nav navbar-nav">
+                    <li><a href="#" id = "pubs" class = "hoverable" style = "color: rgba(51,122,183,1);">Pubs</a></li>
+                  </ul>
+                  <ul class = "nav navbar-nav navbar-right">
+                    <li><a href="#" id = "login" style = "color: rgba(51,122,183,1);">Login</a></li>
+                    <li><a href="#" id = "register" class = "hoverable" style = "color: rgba(51,122,183,1);">Register</a></li>
+                  </ul>
+                </div><!--/.nav-collapse -->
               </div>
-          <?php echo $__env->yieldSection(); ?>
+          </nav>
+          <!-- Sidebar -->
+          <div id="sidebar-wrapper">
+              <nav id="spy">
+                  <ul class="sidebar-nav nav" style = "color: rgba(51,122,183,1);">
+                      <li>
+                          <a href="#"> <span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-inbox"></i></span>Inbox</a>
+                      </li>
+                      <li>
+                          <a href="#"> <span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-globe"></i></span>Notifications</a>
+                      </li>
+                      <li>
+                          <a href="#"><span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-upload"></i></span>Upload File</a>
+                          <ul class="nav-pills nav-stacked" style="list-style-type:none;">
+                              <li><a href="#"><span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-picture"></i></span>Photo</a></li>
+                              <li><a href="#"><span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-film"></i></span>Video</a></li>
+                          </ul>
+                      </li>
+                      <li>
+                          <a href="#"> <span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-user"></i></span>Profile</a>
+                      </li>
+                      <li>
+                          <a href="#"> <span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-cog"></i></span>Settings</a>
+                      </li>
+                      <li>
+                          <a href="#"> <span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-stats"></i></span>Statistics</a>
+                      </li>
+                      <li>
+                          <a href="#"> <span class="fa-stack fa-lg pull-left"><i class="glyphicon glyphicon-log-out"></i></span>Logout</a>
+                      </li>
+                  </ul>
+              </nav>
+          </div>
+          <!-- Page content -->
+          <div class="container-fluid" style = "margin-top: 50px">
 
+              <?php echo $__env->yieldContent('content'); ?>
+
+          </div>
         </div>
 
         <?php $__env->startSection('footer'); ?>
