c5d0857f46c28142681ba5d15fb87d275afe9aa4
Trodrige <tigrodrige@gmail.com>
2016-07-16 13:29:27 +0100

    landing_page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,27 +9,6 @@
 
 @section('content')
 <div class="row">
-    <div class="col-lg-12">
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-    </div>
+        <img src="{{asset('images\land1.jpg')}}" class="img img-responsive" alt="landing_page" height="100%" width="120%">
 </div>
 @endsection
--- storage/framework/views/456c9e62efebad010574d30d2ec4f72c092c80a5.php
+++ storage/framework/views/456c9e62efebad010574d30d2ec4f72c092c80a5.php
@@ -7,28 +7,7 @@
 
 <?php $__env->startSection('content'); ?>
 <div class="row">
-    <div class="col-lg-12">
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-        <h1>Simple Sidebar With Bootstrap 3 by <a href="" >iPub.com</a></h1>
-        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="">create cool simple sidebar menu with boostrap 3</a></p>
-    </div>
+        <img src="<?php echo e(asset('images\land1.jpg')); ?>" class="img img-responsive" alt="landing_page" height="100%" width="120%">
 </div>
 <?php $__env->stopSection(); ?>
 
