d463488fc2410d9a9cd046a45f15975034d8799c
kerick <frukerickjeff@gmail.com>
2016-07-16 14:43:56 +0100

    errors

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -9,7 +9,7 @@ class CreateUsersTable extends Migration
      * Run the migrations.
      *
      * @return void
-     */
+     */ 
     public function up()
     {
         Schema::create('users', function(Blueprint $table){
