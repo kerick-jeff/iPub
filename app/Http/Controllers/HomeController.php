83a1dbdd0137f18028e82465412123a6f41e4759
Trodrige <tigrodrige@gmail.com>
2016-07-17 13:37:45 +0100

    for pull request

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,10 +24,6 @@ class HomeController extends Controller
      */
     public function account()
     {
-<<<<<<< HEAD
-        return view('layout.master');
-=======
         return view('account');
->>>>>>> 177e3125edb530f0bec8de982e405f09c8bdb16a
     }
 }
