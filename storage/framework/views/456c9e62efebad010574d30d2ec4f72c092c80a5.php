cd53a5f26bab53d286b07c84f7cc72023928ace5
Trodrige <tigrodrige@gmail.com>
2016-07-17 10:20:45 +0100

    welcome page

--- storage/framework/views/456c9e62efebad010574d30d2ec4f72c092c80a5.php
+++ storage/framework/views/456c9e62efebad010574d30d2ec4f72c092c80a5.php
@@ -1,13 +1,8 @@
-<?php $__env->startSection('title'); ?>
-
-<?php $__env->startSection('description'); ?>
-    <meta name="description" content = "describe iPub">
-    <meta name="author" content = "name of author">
-<?php $__env->stopSection(); ?>
-
 <?php $__env->startSection('content'); ?>
-<div class="row">
-    <img src="<?php echo e(asset('images/land1.jpg')); ?>" class="img img-responsive" alt="landing_page" height="100%" width="120%">
+<div class="container-fluid" style="margin-left:-70px; margin-right: -15px ">
+    <div class="row">
+        <img src="<?php echo e(asset('images/land1.jpg')); ?>" alt="landing_page" class="img img-responsive" style="max-width:100%; height:auto;">
+    </div>
 </div>
 <?php $__env->stopSection(); ?>
 
