2fdd7a9a64b5a11a0715d35948b671dd8b657c51
kerick-jeff <frukerickjeff@gmail.com>
2018-09-21 12:37:27 +0100

    udpates

--- resources/views/layouts/pubs/master.blade.php
+++ resources/views/layouts/pubs/master.blade.php
@@ -26,7 +26,28 @@
   <div class="content-wrapper" style = "margin-left: 0px; background-color: #ecf0f5">
     <!-- Main content -->
     <section class="content">
-      
+      <div class="row" id = "navigation">
+        <div class="col-md-12">
+          <!-- Mobile navigation -->
+          <div class="box box-primary">
+            <div class="box-body">
+              <div class="btn-group btn-group-justified" role="group" aria-label="Navigation">
+                <div class="btn-group" role="group">
+                  <a href = "#categories" class="btn btn-default truncate">Categories</a>
+                </div>
+                <div class="btn-group" role="group">
+                  <a href = "#pubs" class="btn btn-default truncate">Pubs</a>
+                </div>
+                <div class="btn-group" role="group">
+                  <a href = "#priority-zone" class="btn btn-default truncate">Priority Zone</a>
+                </div>
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+      </div>
 
        @yield('content')
 
